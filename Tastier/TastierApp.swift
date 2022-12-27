11671295e27288f72012ef35377c646cc78d159d
wong-hj <hj.wong02@gmail.com>
2022-12-27 09:14:47 +0800

    Initial Commit

---
+++ Tastier/TastierApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastierApp.swift
+//  Tastier
+//
+//  Created by WONG HORNG JUN on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TastierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
